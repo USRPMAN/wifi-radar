d9668ac789b31210935fbccfa4f45dde189736fe
Ernest Carozza <ernest.carozza@gmail.com>
2014-12-06 16:56:20 -0500

    updated comments in segment_intersect and made more parameters configurable

--- sim/segment_intersect.m
+++ sim/segment_intersect.m
@@ -1,35 +1,53 @@
-% Idea: Ray at point a is moving to point b
-% Does it cross the line segment formed by points ref1 and ref2?
-% return vector does_cross with same length as a, b set to 1 or 0
-% for the ones where it does cross
-% also return angle of line b-a after reflection and the length of (b-a)
-%    minus the length it took it get from a to line segment ref1_ref2
+function [does_cross, cross_points, new_angles, remaining_dist] = ...
+    segment_intersect(pt, delta_pt, wall_pt1, wall_pt2, varargin)
+% Determine if point moving a small increment intersercts a fixed wall
+% If there is an intersection, find the intersection point, the reflection
+% direction, and the amount of distance left to move such that
+% abs(delta_pt) meters are still traveled
 %
-% TODO: instead return new positions after reflection rather than remaining
-% distance
-function [does_cross, cross_points, new_angles, remaining_dist] = segment_intersect(a, b, ref1, ref2)
-% a, b: [size, 2]
-% ref1, ref2: x,y
+% Inputs:
+%   pt: [num_rays, 2] matrix of <x,y> coordinates
+%   delta_pt: [num_rays, 2] matrix of <delta_x, delta_y> vectors
+%   wall: <x_start, x_end, y_start, y_end> representation of wall
+%  
+%  Optional Inputs:
+%   plot: Set to 1 to plot simulation. Defaults to 0
+%
+% Outputs:
+%   does_cross: [num_rays, 1] matrix of whether ray intersected
+%   cross_points: [num_crosses, 2] matrix of points where rays intersected
+%   new_angles: [num_crosses, 1] matrix of ray angles after reflection
+%   remaining_dist: [num_crosses, 1] matrix of distances left after
+%     reflection in the direction of new_angles
+%
 
-r = b - a;
-s = ref2 - ref1;
+ip = inputParser;
+ip.addOptional('plot', 0);
+ip.parse(varargin{:})
+args = ip.Results;
 
-if s(1) < 0
-    s = s*-1;
+% Always have the wall vector pointing in +x direction
+wall_vec = wall_pt2 - wall_pt1;
+if wall_vec(1) < 0
+    wall_vec = wall_vec*-1;
 end
 
-rcs = r(:, 1) .* s(:, 2) - r(:, 2) .* s(:, 1);
+% Find cross product of wall vector and ray movement vector
+xprod = delta_pt(:, 1) .* wall_vec(:, 2) - delta_pt(:, 2) .* wall_vec(:, 1);
 
-qmp = repmat(ref1, [size(a, 1), 1]) - a;
+qmp = repmat(wall_pt1, [size(pt, 1), 1]) - pt;
+t = (qmp(:, 1) .* wall_vec(:, 2) - qmp(:, 2) .* wall_vec(:, 1)) ./ xprod;
+u = (qmp(:, 1) .* delta_pt(:, 2) - qmp(:, 2) .* delta_pt(:, 1)) ./ xprod;
 
-t = (qmp(:, 1) .* s(:, 2) - qmp(:, 2) .* s(:, 1)) ./ rcs;
-u = (qmp(:, 1) .* r(:, 2) - qmp(:, 2) .* r(:, 1)) ./ rcs;
+% The previous two lines will be garbage when the denominator is 0
+% We don't care about those since they are not intersections anyway
+t(xprod == 0) = -1;
+u(xprod == 0) = -1;
 
-t(rcs == 0) = -1;
-u(rcs == 0) = -1;
-
+% if 0 <= t <= 1 and 0 <= u <= 1, the vectors intersect
 does_cross = t<=1 & t>= 0 & u<=1 & u>=0;
 
+% Exit early if no rays intersected
 if sum(does_cross) == 0
     cross_points = [];
     new_angles = [];
@@ -37,18 +55,18 @@ if sum(does_cross) == 0
     return
 end
 
-%cross_points = ref1(does_cross, :) + s(does_cross, :) .* u(does_cross, :);
-cross_points = a(does_cross, :) + r(does_cross, :) .* repmat(t(does_cross, :), [1, 2]);
+cross_points = pt(does_cross, :) + delta_pt(does_cross, :) .* ...
+    repmat(t(does_cross, :), [1, 2]);
 
-move_to_cross = r(does_cross, :) .* repmat(t(does_cross, :), [1, 2]);
+% TODO next 4 lines can be more efficient -- use (1-t)*delta_pt
+move_to_cross = delta_pt(does_cross, :) .* repmat(t(does_cross, :), [1, 2]);
 part1_dist = sqrt(sum(move_to_cross .* move_to_cross, 2));
-total_dist = sqrt(sum(r(does_cross, :) .* r(does_cross, :), 2));
+total_dist = sqrt(sum(delta_pt(does_cross, :) .* delta_pt(does_cross, :), 2));
 remaining_dist = total_dist - part1_dist;
 
-wall_angles = atan2d(s(:, 2), s(:, 1));
-
 % Compute new angles
-old_angles = atan2d(r(does_cross, 2), r(does_cross, 1));
+wall_angles = atan2d(wall_vec(:, 2), wall_vec(:, 1));
+old_angles = atan2d(delta_pt(does_cross, 2), delta_pt(does_cross, 1));
 new_angles = mod(2*wall_angles - old_angles, 360);
 
 % Advance each cross point slightly in the direction of reflection
@@ -57,15 +75,26 @@ new_angles = mod(2*wall_angles - old_angles, 360);
 unit_vectors = [cosd(new_angles), sind(new_angles)];
 cross_points = cross_points + 10^-9 * unit_vectors;
 
-%reflected_positions = cross_points + unit_vectors .* repmat(remaining_dist, [1, 2]);
-
-%figure()
-%hold on
-%line(ref1, ref2)
-%a_cross = a(does_cross, :);
-%line([a_cross(:, 1), cross_points(:, 1)]', [a_cross(:, 2), cross_points(:, 2)]');
-%line([cross_points(:, 1), reflected_positions(:, 1)]', [cross_points(:, 2), reflected_positions(:, 2)]');
-%axis([-8, 8, -8, 8])
-
+if args.plot
+    figure()
+    hold on
+    line(wall_pt1, wall_pt2)
+    a_cross = pt(does_cross, :);
+    ref_pos = cross_points + unit_vectors .* ...
+        repmat(remaining_dist, [1, 2]);
+    if sum(~does_cross) > 0
+        line([pt(~does_cross, 1), ...
+            pt(~does_cross, 1) + delta_pt(~does_cross, 1)]', ...
+            [pt(~does_cross, 2), ...
+            pt(~does_cross, 2) + delta_pt(~does_cross, 2)]');
+    end
+    if sum(does_cross) > 0
+        line([a_cross(:, 1), cross_points(:, 1)]', ...
+            [a_cross(:, 2), cross_points(:, 2)]');
+        line([cross_points(:, 1), ref_pos(:, 1)]', ...
+            [cross_points(:, 2), ref_pos(:, 2)]');
+    end
+    axis([-8, 8, -8, 8])
+end
 
 end
