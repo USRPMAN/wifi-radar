05e3958b0df8082390035d75948540c4046ede5d
Ernest Carozza <ernest.carozza@gmail.com>
2014-12-06 16:55:50 -0500

    added canned test for test_segment_intersect

--- sim/test_segment_intersect.m
+++ sim/test_segment_intersect.m
@@ -4,15 +4,25 @@ a = [-2, -2;
      -4, -2;
      -5, -2;
      -6, -2;
-     -7, -2];
+     -7, -2;
+     5, 2];
 b = [3, 3;
      3, 2;
      2, 2;
      2, 2;
-     2, 2];
+     2, 2;
+     -1, 0];
 ref1 = [0, 1];
 ref2 = [1, 0];
-[does_cross, cross_points, new_angles, remaining_dist] = segment_intersect(a, b, ref1, ref2)
 
+load('test_data/test_segment_intersect.mat');
+
+[does_cross1, cross_points1, new_angles1, remaining_dist1] = ...
+    segment_intersect(a, b-a, ref1, ref2);
+
+assert(all(does_cross == does_cross1));
+assert(all(all(cross_points == cross_points1)));
+assert(all(new_angles == new_angles1));
+assert(all(remaining_dist == remaining_dist1));
 
 end
