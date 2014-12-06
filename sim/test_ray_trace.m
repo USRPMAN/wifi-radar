93a43aabd4e46213cddec1944e14fc67d5ffeacb
Ernest Carozza <ernest.carozza@gmail.com>
2014-12-06 15:58:06 -0500

    added canned test for ray tracing

--- sim/test_ray_trace.m
+++ sim/test_ray_trace.m
@@ -6,7 +6,12 @@ walls = [8, 8, -10, 10;
          16,16,-12, 12];
 target_position = [4, 4];
 
+load('test_data/test_ray_trace.mat');
+expected_result = imp_res;
 
 impulse_response = ray_trace(Tx, Rx, walls, 3e9, target_position);
-%impulse_response
+if sum(abs(impulse_response - expected_result)) > 0
+    error('Test Failed')
+end
+    
 end
