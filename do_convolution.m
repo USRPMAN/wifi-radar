9438476ea152d96886cfbdd75fd13ff6ac285901
Ernest Carozza <ecarozza@mitre.org>
2014-12-09 10:30:35 -0500

    updated simulation

--- do_convolution.m
+++ do_convolution.m
@@ -4,13 +4,17 @@ load('wifidata.mat');
 load('sim/h_data.mat');
 
 % every 1000 samples get new h
-d_out = zeros(20000000+8, 1);
+h_size = size(h, 2);
+iterations = 80;
+d_out = zeros(20000000+h_size-1, 1);
 inc = 0;
-for i=1:10000:20000000-1
+for i=1:iterations:20000000-1
     inc = inc + 1;
-    this_h = h(inc, :);
-    part = conv(d(i:(i+10000-1)), this_h);
-    d_out(i:(i+10006)) = d_out(i:(i+10006)) + part;
+    this_h = h(inc, :)';
+    part = conv(d(i:(i+iterations-1)), this_h);
+    %size(part)
+    %size(d_out(i:(i+iterations+h_size-2)))
+    d_out(i:(i+iterations+h_size-2)) = d_out(i:(i+iterations+h_size-2)) + part;
     
 end
 
--- sim/generate_h.m
+++ sim/generate_h.m
@@ -8,19 +8,24 @@ target_velocity = [4, 0]; % 4 m/s to the right
 
 num_symbols = 20000000;
 time_increments = 1/20e6;
-jumps = 80;
 
 num_iterations = 5;
 dec_rate = 1;
-h = zeros(num_symbols / jumps, num_iterations / dec_rate);
 
+
+jumps=1:80:num_symbols;
+h = zeros(numel(jumps), num_iterations / dec_rate);
 inc = 0;
-for i=1:jumps:num_symbols
+for i=jumps
     inc = inc + 1;
     target = target_position + target_velocity * (i-1) * time_increments;
-    h_orig = ray_trace(Tx, Rx, walls, 20e6, target, 'num_iterations', num_iterations);    
+    h_orig = ray_trace(Tx, Rx, walls, 20e6, target, ...
+        'num_iterations', num_iterations, ...
+        'num_rays', 2, ...
+        'wall_pass_gain', 0.0, ...
+        'wall_refl_gain', 100.0);    
     h(inc, :) = h_orig; %resample(h_orig, 1, dec_rate);
-    if mod(i, 800) == 1
+    if mod(i, 8000) == 1
         i
     end
 end
