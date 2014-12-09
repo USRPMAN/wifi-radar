f574e699a3f9247b7cc428654b0b8578abc3136b
Ernest Carozza <ecarozza@mitre.org>
2014-12-09 11:41:17 -0500

    update generate_h.m

--- sim/generate_h.m
+++ sim/generate_h.m
@@ -1,4 +1,4 @@
-function test_rdm()
+function generate_h()
 
 Tx = [0, 0];
 Rx = [-4.05, 0];
@@ -7,23 +7,22 @@ target_position = [20, 0];
 target_velocity = [4, 0]; % 4 m/s to the right
 
 num_symbols = 20000000;
-time_increments = 1/20e6;
+time_increments = 1./num_symbols;
 
-num_iterations = 5;
+num_taps = 5; 
 dec_rate = 1;
 
-
 jumps=1:80:num_symbols;
-h = zeros(numel(jumps), num_iterations / dec_rate);
+h = zeros(numel(jumps), num_taps / dec_rate);
 inc = 0;
 for i=jumps
     inc = inc + 1;
     target = target_position + target_velocity * (i-1) * time_increments;
     h_orig = ray_trace(Tx, Rx, walls, 20e6, target, ...
-        'num_iterations', num_iterations, ...
-        'num_rays', 2, ...
-        'wall_pass_gain', 0.0, ...
-        'wall_refl_gain', 100.0);    
+        'num_iterations', num_taps, ...
+        'num_rays', 2, ... % Everything along x axis...this makes it faster
+        'wall_pass_gain', 0.5, ...
+        'wall_refl_gain', 0.7);    
     h(inc, :) = h_orig; %resample(h_orig, 1, dec_rate);
     if mod(i, 8000) == 1
         i
